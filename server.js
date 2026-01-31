238343e82875c232ad18c1088ae21b13e7e14484
spy9729 <spy9729@gmail.com>
2026-02-01 00:23:35 +0530

    Fix CORS for custom token header

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ app.use(
     origin: ["http://localhost:5173", "https://clokk-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "token", // ADD THIS
+      "Accept",
+    ],
   }),
 );
+
 app.use(express.json());
 app.use("/api/user", userRouter);
 
